afa96a172aedadf82e84074c8a92306f408fe897
trenzer <118267090+trenzer@users.noreply.github.com>
2022-11-16 15:47:21 +0100

    🤓

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
+
+//why im amazing
+for (var i = 0; i < 100; i++) {
+  const newelement = document.createElement('p');
+  newelement.innerText =
+    i +
+    '. my iq is: ' +
+    'very '.repeat(i) +
+    'high. (it is: ' +
+    i * ((i + 1) * 10) +
+    ')';
+  appDiv.appendChild(newelement);
+}
